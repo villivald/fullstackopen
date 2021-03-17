5f7be23b48224eba03f93bfb6222f6b43a05d288
villivald <maxim.villivald@gmail.com>
2021-03-17 15:07:11 +0200

    tehtävä 1.2

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,17 +1,12 @@
 import React from "react";
+import Part from "./Part";
 
 const Content = (props) => {
   return (
     <>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part={props.part1} exercises={props.exercises1} />
+      <Part part={props.part2} exercises={props.exercises2} />
+      <Part part={props.part3} exercises={props.exercises3} />
     </>
   );
 };
---
+++ part1/courseinfo/src/components/Part.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+export default Part;
