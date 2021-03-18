b7be7f960ea3dd4722c3b21eba0eba32056e9000
villivald <maxim.villivald@gmail.com>
2021-03-18 10:57:24 +0200

    tehtävä 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import Statistics from "./Components/Statistics";
 
 const App = () => {
   const [good, setGood] = useState(0);
@@ -21,13 +22,7 @@ const App = () => {
       <button onClick={handleGoodClick}>Good</button>
       <button onClick={handleNeutralClick}>Neutral</button>
       <button onClick={handleBadClick}>Bad</button>
-      <h1>Statistics</h1>
-      <p>Good {good}</p>
-      <p>Neutral {neutral}</p>
-      <p>Bad {bad}</p>
-      <p>All {good + neutral + bad}</p>
-      <p>Average {(good - bad) / (good + neutral + bad)}</p>
-      <p>Positive {(good / (good + neutral + bad)) * 100}%</p>
+      <Statistics good={good} bad={bad} neutral={neutral} />
     </div>
   );
 };
---
+++ part1/unicafe/src/Components/Statistics.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Statistics = ({ good, neutral, bad }) => {
+  return (
+    <div>
+      <h1>Statistics</h1>
+      <p>Good {good}</p>
+      <p>Neutral {neutral}</p>
+      <p>Bad {bad}</p>
+      <p>All {good + neutral + bad}</p>
+      <p>Average {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive {(good / (good + neutral + bad)) * 100}%</p>
+    </div>
+  );
+};
+
+export default Statistics;
