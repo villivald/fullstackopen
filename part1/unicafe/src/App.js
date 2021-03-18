8f474d4d3bb51db63435098c59649b7cde82561d
villivald <maxim.villivald@gmail.com>
2021-03-18 10:53:57 +0200

    tehtävä 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -25,6 +25,9 @@ const App = () => {
       <p>Good {good}</p>
       <p>Neutral {neutral}</p>
       <p>Bad {bad}</p>
+      <p>All {good + neutral + bad}</p>
+      <p>Average {(good - bad) / (good + neutral + bad)}</p>
+      <p>Positive {(good / (good + neutral + bad)) * 100}%</p>
     </div>
   );
 };
