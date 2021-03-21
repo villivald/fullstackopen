e0275b74664dcd732dbb7b8f6536022b96719395
villivald <maxim.villivald@gmail.com>
2021-03-21 10:11:55 +0200

    tehtävä 2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 0 }]);
+  const [newName, setNewName] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    setPersons(persons.concat(noteObject));
+    setNewName("");
+  };
+
+  const handleChange = (e) => {
+    setNewName(e.target.value);
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input onChange={handleChange} value={newName} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <p key={person.id}>{person.name}</p>
+      ))}
+    </div>
+  );
+};
+
+export default App;
