5012943d2c2aa621b14b55e5ac8fc0aa66fd34c0
villivald <maxim.villivald@gmail.com>
2021-03-23 13:44:28 +0200

    tehtävä 2.14 1/2

--- part2/countries/src/components/CountryInfo.js
+++ part2/countries/src/components/CountryInfo.js
@@ -1,35 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Weather from "./Weather";
 
 const CountryInfo = ({ countries, filter }) => {
-  return (
-    <div>
-      {countries
-        .filter((country) =>
-          country.name.toLowerCase().includes(filter.toLowerCase())
-        )
-        .map((country) => (
-          <div key={country.numericCode}>
-            {
-              <div>
-                <h1>{country.name}</h1>
-                <p>Capital {country.capital}</p>
-                <p>Population {country.population}</p>
-                <h1>Languages</h1>
-                <ul>
-                  {country.languages.map((lang) => (
-                    <li key={lang.iso639_1}>{lang.name}</li>
-                  ))}
-                </ul>
-                <img
-                  style={{ width: "20%" }}
-                  src={country.flag}
-                  alt={country.name}
-                />
-              </div>
-            }
-          </div>
-        ))}
+  const [countryList, setCountryList] = useState([]);
+  const [capital, setCapital] = useState(null);
+
+  useEffect(() => {
+    setCountryList(
+      countries.filter((country) =>
+        country.name.toLowerCase().includes(filter.toLowerCase())
+      )
+    );
+  }, [countries, filter]);
+
+  useEffect(() => {
+    if (countryList.length === 1) {
+      setCapital(countryList[0].capital);
+    }
+  }, [countryList]);
+
+  const country = countryList.map((country) => (
+    <div key={country.numericCode}>
+      <div>
+        <h1>{country.name}</h1>
+        <p>Capital {country.capital}</p>
+        <p>Population {country.population}</p>
+        <h1>Languages</h1>
+        <ul>
+          {country.languages.map((lang) => (
+            <li key={lang.iso639_1}>{lang.name}</li>
+          ))}
+        </ul>
+        <img style={{ width: "20%" }} src={country.flag} alt={country.name} />
+      </div>
     </div>
+  ));
+
+  return (
+    <>
+      {country}
+      {capital ? <Weather city={capital} /> : <p>Loading</p>}
+    </>
   );
 };
 
---
+++ part2/countries/src/components/Weather.js
@@ -0,0 +1,55 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Weather = ({ city }) => {
+  const [weather, setWeather] = useState(null);
+
+  useEffect(() => {
+    let source = axios.CancelToken.source();
+    axios
+      .get("http://api.weatherstack.com/current", {
+        params: {
+          access_key: "7d61cadc6c7a4731542f76f99aee572d",
+          query: city,
+        },
+        cancelToken: source.token,
+      })
+      .catch((error) => {
+        if (axios.isCancel(error)) {
+          console.log("Request canceled", error.message);
+        } else {
+          throw error;
+        }
+      })
+      .then((response) => {
+        if (response.statusText === "OK") {
+          setWeather(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error.config);
+      });
+
+    return () => {
+      source.cancel("Weather component is unmounting");
+    };
+  }, [city]);
+
+  if (weather) {
+    return (
+      <div>
+        <p>Temperature: {weather.current.temperature}</p>
+        <img src={weather.current.weather_icons[0]} alt="weather" />
+        <p>
+          Wind: {weather.current.wind_speed} mph direction{" "}
+          {weather.current.wind_direction}
+        </p>
+      </div>
+    );
+  } else {
+    console.log(weather);
+    return <div></div>;
+  }
+};
+
+export default Weather;
