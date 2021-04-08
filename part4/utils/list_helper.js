c45d2756b306ac6e781e6c6cd1306ace4f0ce53e
villivald <maxim.villivald@gmail.com>
2021-04-08 12:00:39 +0300

    tehtävä 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -6,6 +6,13 @@ const dummy = (blogs) => {
   }
 };
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes;
+  }, 0);
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
