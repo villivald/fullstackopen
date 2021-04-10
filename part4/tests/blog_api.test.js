23db7a0610b87e16add289146135d42fb4131141
villivald <maxim.villivald@gmail.com>
2021-04-10 13:04:42 +0300

    tehtävä 4.10

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -6,6 +6,7 @@ const api = supertest(app);
 
 const Blog = require("../models/blog");
 
+// BEFORE EACH
 beforeEach(async () => {
   await Blog.deleteMany({});
 
@@ -15,6 +16,7 @@ beforeEach(async () => {
   }
 });
 
+// ALL === JSON
 test("blogs are returned as json", async () => {
   await api
     .get("/api/blogs")
@@ -22,18 +24,21 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 });
 
+// RIGHT LENGTH
 test("there are right amount of blogs", async () => {
   const response = await api.get("/api/blogs");
 
   expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
+// UNIQUE ID
 test("blog has a unique id named 'id'", async () => {
   const response = await api.get("/api/blogs");
 
   expect(response.body[0].id).toBeDefined();
 });
 
+// SPECIFIC TITLE
 test("a specific title is within the returned blogs", async () => {
   const response = await api.get("/api/blogs");
 
@@ -41,6 +46,7 @@ test("a specific title is within the returned blogs", async () => {
   expect(contents).toContain("My New Blog");
 });
 
+// CAN ADD A NEW BLOG
 test("a valid blog can be added", async () => {
   const newBlog = {
     title: "A Brilliant Blog",
@@ -55,13 +61,15 @@ test("a valid blog can be added", async () => {
     .expect(200)
     .expect("Content-Type", /application\/json/);
 
-  const blogsAtEnd = await helper.blogsInDb();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+  const blogsAfterAdding = await helper.blogsInDb();
 
-  const contents = blogsAtEnd.map((n) => n.title);
+  const contents = blogsAfterAdding.map((blog) => blog.title);
   expect(contents).toContain("A Brilliant Blog");
+
+  expect(blogsAfterAdding).toHaveLength(helper.initialBlogs.length + 1);
 });
 
+// BLOG WITHOUT CONTENT IS NOT OK
 test("blog without content is not added", async () => {
   const newBlog = {};
 
@@ -71,6 +79,7 @@ test("blog without content is not added", async () => {
   expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
 });
 
+// CAN VIEW SINGLE BLOG
 test("a specific blog can be viewed", async () => {
   const blogsAtStart = await helper.blogsInDb();
   const blogToView = blogsAtStart[0];
@@ -84,6 +93,7 @@ test("a specific blog can be viewed", async () => {
   expect(resultBlog.body).toEqual(processedBlogToView);
 });
 
+// CAN DELETE A BLOG
 test("a blog can be deleted", async () => {
   const blogsAtStart = await helper.blogsInDb();
   const blogToDelete = blogsAtStart[0];
