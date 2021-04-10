8b2369eb8866bbb0dc2113678bf18eb83901b2fc
villivald <maxim.villivald@gmail.com>
2021-04-10 18:06:23 +0300

    fix validation

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -21,12 +21,6 @@ blogsRouter.get("/:id", async (request, response) => {
 blogsRouter.post("/", async (request, response) => {
   const body = request.body;
 
-  if (body.title === undefined) {
-    return response.status(400).json({ error: "title missing" });
-  } else if (body.url === undefined) {
-    return response.status(400).json({ error: "url missing" });
-  }
-
   const blog = new Blog({
     title: body.title,
     author: body.author,
