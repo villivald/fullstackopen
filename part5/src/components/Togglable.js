fedfe3f11997a7bda5533041be590af02d9f6f72
villivald <maxim.villivald@gmail.com>
2021-04-25 17:30:46 +0300

    tehtävä 5.11

--- part5/src/components/Togglable.js
+++ part5/src/components/Togglable.js
@@ -1,4 +1,5 @@
 import React, { useState, useImperativeHandle } from "react";
+import PropTypes from "prop-types";
 
 const Togglable = React.forwardRef((props, ref) => {
   const [visible, setVisible] = useState(false);
@@ -26,5 +27,10 @@ const Togglable = React.forwardRef((props, ref) => {
     </div>
   );
 });
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired,
+  cancelButtonLabel: PropTypes.string.isRequired,
+};
 
 export default Togglable;
