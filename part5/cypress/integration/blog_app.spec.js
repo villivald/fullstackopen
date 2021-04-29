f67b71e7d216d940361755aa656fac3f3a876024
villivald <maxim.villivald@gmail.com>
2021-04-29 11:00:49 +0300

    tehtävä 5.18

--- part5/cypress/integration/blog_app.spec.js
+++ part5/cypress/integration/blog_app.spec.js
@@ -18,15 +18,31 @@ describe("Note app", function () {
     cy.contains("Log in").click();
   });
 
-  it("user can login", function () {
-    cy.contains("Log in").click();
-    cy.get("#username").type("theking");
-    cy.get("#password").type("lalakers");
-    cy.get("#login-button").click();
-    cy.contains("Lebron James is logged in");
+  describe("Login", function () {
+    it("user can login with correct credentials", function () {
+      cy.contains("Log in").click();
+      cy.get("#username").type("theking");
+      cy.get("#password").type("lalakers");
+      cy.get("#login-button").click();
+      cy.contains("Lebron James is logged in");
+      cy.get(".notification").should(
+        "have.css",
+        "border-color",
+        "rgb(0, 128, 0)"
+      );
+    });
+
+    it("login fails with wrong credentials", function () {
+      cy.contains("Log in").click();
+      cy.get("#username").type("uncledrew");
+      cy.get("#password").type("biggysmalls");
+      cy.get("#login-button").click();
+      cy.contains("Wrong username or password");
+      cy.get(".warning").should("have.css", "border-color", "rgb(255, 0, 0)");
+    });
   });
 
-  describe("when logged in", function () {
+  describe("When logged in", function () {
     beforeEach(function () {
       cy.contains("Log in").click();
       cy.get("#username").type("theking");
