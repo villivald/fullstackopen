8c154214b3b2d926b8a431d132146cb4dce9b8d3
villivald <maxim.villivald@gmail.com>
2021-05-13 15:48:16 +0300

    tehtävä 7.4

--- part7/routed-anecdotes/src/components/CreateNew.js
+++ part7/routed-anecdotes/src/components/CreateNew.js
@@ -1,19 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import { useHistory } from "react-router-dom";
+import { useField } from "../hooks/index";
 
 const CreateNew = (props) => {
-  const [content, setContent] = useState("");
-  const [author, setAuthor] = useState("");
-  const [info, setInfo] = useState("");
-
   const history = useHistory();
+
+  const content = useField("text");
+  const author = useField("text");
+  const info = useField("text");
 
   const handleSubmit = (e) => {
     e.preventDefault();
     props.addNew({
-      content,
-      author,
-      info,
+      content: content.value,
+      author: author.value,
+      info: info.value,
       votes: 0,
     });
     history.push("/");
@@ -21,33 +22,54 @@ const CreateNew = (props) => {
 
   return (
     <div>
-      <h2>create a new anecdote</h2>
+      <h2>Create a new anecdote</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          content
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            gap: "21px",
+            marginBottom: "10px",
+          }}
+        >
+          Content:{" "}
           <input
-            name="content"
-            value={content}
-            onChange={(e) => setContent(e.target.value)}
+            type={content.type}
+            value={content.value}
+            onChange={content.onChange}
           />
         </div>
-        <div>
-          author
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            gap: "26px",
+            marginBottom: "10px",
+          }}
+        >
+          Author:{" "}
           <input
-            name="author"
-            value={author}
-            onChange={(e) => setAuthor(e.target.value)}
+            type={author.type}
+            value={author.value}
+            onChange={author.onChange}
           />
         </div>
-        <div>
-          url for more info
-          <input
-            name="info"
-            value={info}
-            onChange={(e) => setInfo(e.target.value)}
-          />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            gap: "50px",
+            marginBottom: "10px",
+          }}
+        >
+          Url:{" "}
+          <input type={info.type} value={info.value} onChange={info.onChange} />
         </div>
-        <button>create</button>
+
+        <button style={{ marginBottom: "10px" }}>Create</button>
       </form>
     </div>
   );
--- part7/routed-anecdotes/src/components/Footer.js
+++ part7/routed-anecdotes/src/components/Footer.js
@@ -1,14 +1,19 @@
 const Footer = () => (
   <div>
-    Anecdote app for{" "}
-    <a href="https://courses.helsinki.fi/fi/tkt21009">
-      Full Stack -websovelluskehitys
-    </a>
-    . See{" "}
-    <a href="https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js">
-      https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js
-    </a>{" "}
-    for the source code.
+    <div>
+      Anecdote app for{" "}
+      <a href="https://courses.helsinki.fi/fi/tkt21009">
+        Full Stack -websovelluskehitys
+      </a>
+      .
+    </div>
+    <div>
+      See{" "}
+      <a href="https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js">
+        https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js
+      </a>{" "}
+      for the source code.
+    </div>
   </div>
 );
 
