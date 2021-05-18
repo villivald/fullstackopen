df5f50f689a1d618117aa6e803bcb0b8e3b45918
villivald <maxim.villivald@gmail.com>
2021-05-18 21:48:54 +0300

    finish webpack part

--- part7/webpack/webpack.config.js
+++ part7/webpack/webpack.config.js
@@ -1,22 +1,41 @@
 const path = require("path");
+const webpack = require("webpack");
 
-const config = {
-  entry: ["@babel/polyfill", "./src/index.js"],
-  output: {
-    path: path.resolve(__dirname, "build"),
-    filename: "main.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        loader: "babel-loader",
-        options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
-          compact: false,
+const config = (env, argv) => {
+  const backend_url =
+    argv.mode === "production"
+      ? "http://localhost:3001/notes"
+      : "https://evening-mesa-82321.herokuapp.com/api/books";
+
+  return {
+    entry: ["@babel/polyfill", "./src/index.js"],
+    output: {
+      path: path.resolve(__dirname, "build"),
+      filename: "main.js",
+    },
+    devServer: {
+      contentBase: path.resolve(__dirname, "build"),
+      compress: true,
+      port: 8000,
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            compact: false,
+          },
         },
-      },
+        { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      ],
+    },
+    plugins: [
+      new webpack.DefinePlugin({ BACKEND_URL: JSON.stringify(backend_url) }),
     ],
-  },
+  };
 };
+
 module.exports = config;
